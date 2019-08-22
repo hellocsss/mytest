05a2bf39a23f1d88be6aeb90f2a73fc3777610e6
hellocss <50293155+hellocsss@users.noreply.github.com>
2019-08-22 12:13:40 +0800

    Update test.java

--- com_itcast/src/main/java/test.java
+++ com_itcast/src/main/java/test.java
@@ -5,6 +5,6 @@ public class test {
         System.out.println("hello word");
         System.out.println("hello word");
         System.out.println("hello word");
-        System.out.println("hello word");
+        System.out.println("hello");
     }
 }
